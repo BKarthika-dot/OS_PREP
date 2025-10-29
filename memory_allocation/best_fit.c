18c2a06897708dc0d8c368fc19e48ea284f53606
Karthika Bala <bkarthika1203@gmail.com>
2025-10-29 14:37:16 +0530

    Best fit Memory Allocation

---
+++ memory_allocation/best_fit.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#define max 3
+
+int main(){
+    printf("enter no of blocks ");
+    int n;
+    scanf("%d",&n);
+    int blockSize[n];
+    int blockAllocated[n];
+    for(int i=0;i<n;i++){
+        printf("enter size of block %d ",i);
+        scanf("%d",&blockSize[i]);
+        blockAllocated[i]=0;  //free block
+    }
+
+
+    int processSize[max],processAllocated[max];
+
+    for(int i=0;i<max;i++){
+        printf("enter size of process %d ",i);
+        scanf("%d",&processSize[i]);
+        processAllocated[i]=-1;  //not allocated process
+    }
+
+    for(int i=0;i<max;i++){
+        int bestIdx=-1;
+        for(int j=0;j<n;j++){
+            if(!blockAllocated[j]&&blockSize[j]>=processSize[i]){
+                if(bestIdx==-1 || blockSize[bestIdx]>blockSize[j]){
+                    bestIdx=j;
+                }
+            }
+        }
+        if(bestIdx!=-1){
+            processAllocated[i]=bestIdx;
+            blockAllocated[bestIdx]=1;
+        }
+    }
+    printf("Process Number\tProcess Size\tAllocated Block\n");
+
+    for(int i=0;i<max;i++){
+        printf("%d\t\t%d\t\t",i,processSize[i]);
+        if(processAllocated[i]==-1){
+            printf("Not allocated\n");
+        }
+        else{
+            printf("%d\n",processAllocated[i]);
+        }
+    }
+}
