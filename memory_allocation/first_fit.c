2c896708e22a3bd558ea1fd397daff0f4495598f
Karthika Bala <bkarthika1203@gmail.com>
2025-10-29 14:27:21 +0530

    First Fit memory allocation

---
+++ memory_allocation/first_fit.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#define max 3  //no of processes
+int main(){
+    printf("enter no of blocks ");
+    int n;
+    scanf("%d",&n);
+    int blockSize[n];
+    int blockAllocated[n];
+    for(int i=0;i<n;i++){
+        printf("enter size of block %d ",i);
+        scanf("%d",&blockSize[i]);
+        blockAllocated[i]=0;  //free block
+    }
+
+
+    int processSize[max],processAllocated[max];
+
+    for(int i=0;i<max;i++){
+        printf("enter size of process %d ",i);
+        scanf("%d",&processSize[i]);
+        processAllocated[i]=-1;  //not allocated process
+    }
+
+    for(int i=0;i<max;i++){
+        for(int j=0;j<n;j++){
+            if(blockSize[j]>=processSize[i]&&!blockAllocated[j]){
+                processAllocated[i]=j;
+                blockAllocated[j]=1;  //mark block as allocated
+                break;
+            }
+        }
+    }
+
+    printf("Process Number\tProcess Size\tAllocated Block\n");
+
+    for(int i=0;i<max;i++){
+        printf("%d\t\t%d\t\t",i,processSize[i]);
+        if(processAllocated[i]==-1){
+            printf("Not allocated\n");
+        }
+        else{
+            printf("%d\n",processAllocated[i]);
+        }
+    }
+}
